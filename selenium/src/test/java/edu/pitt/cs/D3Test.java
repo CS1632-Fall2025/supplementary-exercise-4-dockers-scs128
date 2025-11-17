3e3a06e32ef24d7a3484b9786e83a8dceb929b07
Scott Sullivan <scs128@pitt.edu>
2025-11-17 15:24:37 -0500

    Added headless chrome option

--- selenium/src/test/java/edu/pitt/cs/D3Test.java
+++ selenium/src/test/java/edu/pitt/cs/D3Test.java
@@ -40,7 +40,9 @@ public class D3Test {
   @Before
 public void setUp() {
     WebDriverManager.chromedriver().setup();
-    driver = new ChromeDriver();
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--headless");
+    driver = new ChromeDriver(options);
     js = (JavascriptExecutor) driver;
     vars = new HashMap<String, Object>();
 }
